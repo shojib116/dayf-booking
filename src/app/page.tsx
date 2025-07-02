4b0e53fffdc6fa1d959c5b3b48c26ebd61d5141b
shojib116 <Md. Nazmul Hasan Shajib>
2025-07-02 15:05:03 +0600

    added hotel-details section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import HotelDetails from "@/components/layout/main/hotel-details";
 import HotelPreview from "@/components/layout/main/hotel-preview";
 
 export default function Home() {
   return (
     <main className="mt-10 font-sans">
       <HotelPreview />
+      <HotelDetails />
     </main>
   );
 }
